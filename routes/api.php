ec08bbe8bbd33370631bb6f8a66c6089e98b6683
Suresh Ramani <ramani.suresh41@gmail.com>
2022-06-06 02:31:21 +0530

    Changes

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
 
-Route::middleware('auth:sanctum')->get('/user123', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
