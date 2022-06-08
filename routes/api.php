8e7bc4847349facfc52552265563ab8553dcf23e
Suresh Ramani <ramani.suresh41@gmail.com>
2022-06-09 00:13:47 +0530

    changes

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\CommentController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+//
 Route::post('/login',[AuthController::class,'login']);
 Route::post('/register',[AuthController::class,'register']);
 
